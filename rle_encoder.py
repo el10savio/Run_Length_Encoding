c1eceb30efd3acfba499aad52743fcdfb74b7e23
Elton SV <el10savio96@gmail.com>
2017-01-06 14:08:11 +0530

    Create rle_encoder.py

---
+++ rle_encoder.py
@@ -0,0 +1,28 @@
+#Run Length Encoding - Encoder
+import sys
+
+a = str(raw_input('Enter String: '))
+print 'Stats:'
+print 'Length=',len(a)
+print 'Size=',sys.getsizeof(a)
+a= list(a)
+
+k=[]
+num=0
+counter=0
+x=a[0]
+while(counter<len(a)):
+    if x==a[counter]:
+      num+=1
+    else:
+      k.extend([num,a[counter-1]])
+      num=1
+    x=a[counter]
+    counter+=1
+k.extend([num,a[counter-1]])
+
+k = ''.join(str(e) for e in k)
+print '\nEncoded- ',k
+print 'Stats:'
+print 'Length=',len(k)
+print 'Size=',sys.getsizeof(k)
